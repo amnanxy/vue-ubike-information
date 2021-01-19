15d271f2eedd644bd7a1c6ce80ec37b9b5217b4c
amnanxy <amnanxy@yahoo.com.tw>
2021-01-19 17:08:13 +0800

    UbikeTable Component

--- ubike-app/src/UBike.vue
+++ ubike-app/src/UBike.vue
@@ -5,51 +5,25 @@
     站點名稱搜尋: <input type="text" v-model="searchStop">
   </p>
 
-  <table class="table table-striped">
-    <thead>
-    <tr>
-      <th>#</th>
-      <th>場站名稱</th>
-      <th>場站區域</th>
-      <th>
-        <span class="link" @click="sortBy('sbi')">目前可用車輛</span>
-      </th>
-      <th>
-        <span class="link" @click="sortBy('tot')">總停車格</span>
-      </th>
-      <th>資料更新時間</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="s in pagingStops" :key="s.sno">
-      <td>{{ s.sno }}</td>
-      <td>{{ s.sna }}</td>
-      <td>{{ s.sarea }}</td>
-      <td>{{ s.sbi }}</td>
-      <td>{{ s.tot }}</td>
-      <td>{{ timeFormat(s.mday) }}</td>
-    </tr>
-    </tbody>
-  </table>
+  <ubike-table :stops="filteredStops" :stop-start-idx="stopStartIdx" :stop-end-idx="stopEndIdx"></ubike-table>
   <pagination :total="total" :page-size="20" @update="showItem"></pagination>
 </template>
 
 <script>
+import UbikeTable from "@/components/UbikeTable";
 import Pagination from "@/components/Pagination";
 
 export default {
   name: "UBike",
   components: {
-    pagination: Pagination
+    ubikeTable: UbikeTable,
+    pagination: Pagination,
   },
   data() {
     return {
       ubikeStops: [],
 
       searchStop: '',
-      
-      isAscSort: true,
-      sortProp: '',
 
       total: 0,
       stopStartIdx: 0,
@@ -57,25 +31,6 @@ export default {
     }
   },
   methods: {
-    timeFormat(t) {
-      var date = [], time = [];
-
-      date.push(t.substr(0, 4));
-      date.push(t.substr(4, 2));
-      date.push(t.substr(6, 2));
-      time.push(t.substr(8, 2));
-      time.push(t.substr(10, 2));
-      time.push(t.substr(12, 2));
-
-      return date.join("/") + ' ' + time.join(":");
-    },
-    sortBy(prop) {
-      if (this.sortProp !== prop) {
-        this.sortProp = prop;
-      } else {
-        this.isAscSort = !this.isAscSort;
-      }
-    },
     showItem(startIdx, endIdx) {
       this.stopStartIdx = startIdx;
       this.stopEndIdx = endIdx;
@@ -90,23 +45,6 @@ export default {
     filteredStops() {
       return this.ubikeStops.filter(stop => stop.sna.includes(this.searchStop));
     },
-    sortedStops() {
-      let prop = this.sortProp;
-      let stops = [...this.filteredStops];
-
-      if (!prop) {
-        return stops;
-      }
-
-      if (this.isAscSort) {
-        return stops.sort((a, b) => a[prop] - b[prop]);
-      } else {
-        return stops.sort((a, b) => b[prop] - a[prop]);
-      }
-    },
-    pagingStops() {
-      return this.sortedStops.slice(this.stopStartIdx, this.stopEndIdx);
-    }
   },
   created() {
 
---
+++ ubike-app/src/components/UbikeTable.vue
@@ -0,0 +1,98 @@
+<template>
+  <table class="table table-striped">
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>場站名稱</th>
+      <th>場站區域</th>
+      <th>
+        <span class="link" @click="sortBy('sbi')">目前可用車輛</span>
+      </th>
+      <th>
+        <span class="link" @click="sortBy('tot')">總停車格</span>
+      </th>
+      <th>資料更新時間</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="s in pagingStops" :key="s.sno">
+      <td>{{ s.sno }}</td>
+      <td>{{ s.sna }}</td>
+      <td>{{ s.sarea }}</td>
+      <td>{{ s.sbi }}</td>
+      <td>{{ s.tot }}</td>
+      <td>{{ timeFormat(s.mday) }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "UbikeTable",
+  props: {
+    stops: {
+      type: Array,
+      required: true
+    },
+    stopStartIdx: {
+      type: Number,
+      required: true
+    },
+    stopEndIdx: {
+      type: Number,
+      required: true
+    },
+  },
+  data() {
+    return {
+      isAscSort: true,
+      sortProp: '',
+    }
+  },
+  computed: {
+    sortedStops() {
+      let prop = this.sortProp;
+      let stops = [...this.stops];
+
+      if (!prop) {
+        return stops;
+      }
+
+      if (this.isAscSort) {
+        return stops.sort((a, b) => a[prop] - b[prop]);
+      } else {
+        return stops.sort((a, b) => b[prop] - a[prop]);
+      }
+    },
+    pagingStops() {
+      return this.sortedStops.slice(this.stopStartIdx, this.stopEndIdx);
+    }
+  },
+  methods: {
+    timeFormat(t) {
+      var date = [], time = [];
+
+      date.push(t.substr(0, 4));
+      date.push(t.substr(4, 2));
+      date.push(t.substr(6, 2));
+      time.push(t.substr(8, 2));
+      time.push(t.substr(10, 2));
+      time.push(t.substr(12, 2));
+
+      return date.join("/") + ' ' + time.join(":");
+    },
+    sortBy(prop) {
+      if (this.sortProp !== prop) {
+        this.sortProp = prop;
+      } else {
+        this.isAscSort = !this.isAscSort;
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
